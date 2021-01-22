693944bd82c510a82183d69749103512a1250c4f
AkashHDubey <52946580+AkashHDubey@users.noreply.github.com>
2021-01-22 20:10:40 +0530

    Update AbstractBoundedQueue.java

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -11,8 +11,8 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     public int fillCount(){
         return fillCount;
     }
-    public abstract T peek();
-    public abstract T dequeue();
-    public abstract void enqueue(T x);
+//     public abstract T peek();
+//     public abstract T dequeue();
+//     public abstract void enqueue(T x);
 
 }
